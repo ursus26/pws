410e6927602698afe2aac8b3e6d313fee4f0bfbf
Bart de Haan <Bart.deHaan@hotmail.com>
2014-11-07 20:31:19 +0100

    Camara update
    werkt nog niet maar begin is gelegt

---
+++ source/Assets/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform Player; 
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveCamera ();
+	}
+
+	void MoveCamera(){
+		var x = transform.position.x; 
+		var y = transform.position.y;
+
+		//x = Player.position.x;
+		//y = Player.position.y;
+
+		transform.position = new Vector2 (x, y);
+	}
+}
--- source/Assets/PlayerMovement.cs
+++ source/Assets/PlayerMovement.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,10 +12,10 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Move ();
+		MovePlayer ();
 	}
 
-	void Move(){
+	void MovePlayer(){
 		if (Input.GetKey (KeyCode.D) | Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate(Vector2.right * 5f * Time.deltaTime);
 		}
