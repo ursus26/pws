20154974e185d485cb47e1e7f217e761a4a0a789
Bart de Haan <Bart.deHaan@hotmail.com>
2014-12-03 19:38:00 +0100

    control update #1
    - deleted camera movement
    - added rotation to player (not finished)

--- source/Assets/PlayerMovement.cs
+++ source/Assets/PlayerMovement.cs
@@ -4,15 +4,23 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 
+	public float test;
+	public float a;
+	public float b;
+	public float c;
 
+
+	public float angle;
+	Vector2 dir;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		MovePlayer ();
+		RotatePlayer ();
 	}
 
 	void MovePlayer(){
@@ -32,5 +40,42 @@ public class PlayerMovement : MonoBehaviour {
 			transform.Translate(Vector2.up * -5f * Time.deltaTime);
 		}
 }
+
+	void RotatePlayer(){
+
+	/*
+
+		a = Input.mousePosition.x - transform.position.x;
+		b = Input.mousePosition.y - transform.position.y;
+		c = Mathf.Cos (a);
+		test = Mathf.Sin (b);
+		a = c * test;
+
+
+		b = Input.mousePosition.y - a;
+		test = Input.mousePosition.x - c;
+
+		if (b == 0) {
+
+		} else {
+			c = Mathf.Sin (b) * 4; // Mathf.Cos (test);
+			transform.Rotate (0, 0, -c);
+		}
+
+
+
+
+		a = Input.mousePosition.y;
+		c = Input.mousePosition.x;
+
+*/
+		dir = Input.mousePosition - transform.position;
+		angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+		}
+
+
 
 }
