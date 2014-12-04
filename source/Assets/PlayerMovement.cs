1ef8803a54620fe8e59f2c0657f7cd68ee629b12
ursus26 <Bart.deHaan@hotmail.com>
2014-12-04 12:10:55 +0100

    Controll update #2
    - Changed rotation algorithm (Needs testing)
    - Added comments

--- source/Assets/PlayerMovement.cs
+++ source/Assets/PlayerMovement.cs
@@ -2,19 +2,17 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
-
-	public float test;
-	public float a;
-	public float b;
-	public float c;
-
-
+	//variables used in this class
+	public float dx;
+	public float dy;
+	public float LastMousePosx;
+	public float LastMousePosy;
 	public float angle;
-	Vector2 dir;
+
 	// Use this for initialization
 	void Start () {
-
+		LastMousePosx = Input.mousePosition.x;
+		LastMousePosy = Input.mousePosition.y;
 	}
 	
 	// Update is called once per frame
@@ -24,57 +22,43 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void MovePlayer(){
-		if (Input.GetKey (KeyCode.D) | Input.GetKey (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.D) | Input.GetKey (KeyCode.RightArrow)) {//if D or Right arrow is pressed->go right
 			transform.Translate(Vector2.right * 5f * Time.deltaTime);
 		}
 		
-		if (Input.GetKey (KeyCode.A) | Input.GetKey (KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.A) | Input.GetKey (KeyCode.LeftArrow)) {//if A or Left arrow is pressed->go left
 			transform.Translate(Vector2.right * -5f * Time.deltaTime);
 		}
 		
-		if (Input.GetKey (KeyCode.W) | Input.GetKey (KeyCode.UpArrow)) {
+		if (Input.GetKey (KeyCode.W) | Input.GetKey (KeyCode.UpArrow)) {//if W or Up arrow is pressed->go up
 			transform.Translate(Vector2.up * 5f * Time.deltaTime);
 		}
 		
-		if (Input.GetKey (KeyCode.S) | Input.GetKey (KeyCode.DownArrow) ) {
+		if (Input.GetKey (KeyCode.S) | Input.GetKey (KeyCode.DownArrow) ) {//if S or Down arrow is pressed->go down
 			transform.Translate(Vector2.up * -5f * Time.deltaTime);
 		}
 }
 
 	void RotatePlayer(){
-
-	/*
-
-		a = Input.mousePosition.x - transform.position.x;
-		b = Input.mousePosition.y - transform.position.y;
-		c = Mathf.Cos (a);
-		test = Mathf.Sin (b);
-		a = c * test;
-
-
-		b = Input.mousePosition.y - a;
-		test = Input.mousePosition.x - c;
-
-		if (b == 0) {
-
-		} else {
-			c = Mathf.Sin (b) * 4; // Mathf.Cos (test);
-			transform.Rotate (0, 0, -c);
-		}
-
-
-
-
-		a = Input.mousePosition.y;
-		c = Input.mousePosition.x;
-
-*/
-		dir = Input.mousePosition - transform.position;
-		angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-
-		}
+		//calculate the difference of the mouse movement in y and x direction 
+		dx = (Input.mousePosition.x - LastMousePosx) / Screen.width;
+		dy = (Input.mousePosition.y - LastMousePosy) / Screen.height;
+		
+		//calculate the angle the player has to rotate
+		if(dx == 0) { //check if dx == 0 or else there will be an error because of deviding by 0
+			dx = 1;	
+		} 
+		angle = Mathf.Atan(dy/dx);
+		
+		//rotate player
+	
+		transform.rotation = Quaternion.Euler(0,0,angle);
+		
+		//save current frame mouse position
+		LastMousePosx = Input.mousePosition.x;
+		LastMousePosy = Input.mousePosition.y;
+		
+	}
 
 
 
