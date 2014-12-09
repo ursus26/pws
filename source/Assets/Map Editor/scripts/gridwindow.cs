bae50a8d6e59ab3ed7a5b538873d99120f4af533
Tijsdv <tijsdv@hotmail.nl>
2014-12-09 21:53:44 +0100

    Map Editor

---
+++ source/Assets/Map Editor/scripts/gridwindow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GridWindow : EditorWindow {
+	grid grid;
+	
+	public void init(){
+		grid = (grid)FindObjectOfType (typeof(grid));
+	}
+	
+	void OnGUI(){
+		grid.color = EditorGUILayout.ColorField(grid.color,GUILayout.Width(200));
+	}		
+}
