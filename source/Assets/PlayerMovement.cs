b811997ef18b0d0555763a22ba9cda0e19ebb561
friso777 <friso777@gmail.com>
2014-12-22 12:44:15 +0100

    update
    update

--- source/Assets/PlayerMovement.cs
+++ source/Assets/PlayerMovement.cs
@@ -2,20 +2,12 @@
 using System.Collections;
 
 
-[RequireComponent (typeof(CharacterController))]
+
 public class PlayerMovement : MonoBehaviour {
-
-	public float rotationSpeed = 450;
-
-	private Quaternion PlayerRotation;
-	private CharacterController controller;
-	private Camera cam;
-
 
 	// Use this for initialization
 	void Start () {
-		controller = GetComponent<CharacterController> ();
-		cam = Camera.main;
+
 	}
 	
 	// Update is called once per frame
@@ -24,34 +16,31 @@ public class PlayerMovement : MonoBehaviour {
 		RotatePlayer ();
 	}
 
+
 	void MovePlayer(){
-		if (Input.GetKey (KeyCode.D) | Input.GetKey (KeyCode.RightArrow)) {//if D or Right arrow is pressed->go right
-			transform.Translate(Vector3.right * 3f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.D) | Input.GetKey (KeyCode.RightArrow)) {			//if D or Right arrow is pressed->move right in the world
+			transform.Translate(Vector3.right * 4f * Time.deltaTime, Space.World);
 		}
 		
-		if (Input.GetKey (KeyCode.A) | Input.GetKey (KeyCode.LeftArrow)) {//if A or Left arrow is pressed->go left
-			transform.Translate(Vector3.right * -3f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.A) | Input.GetKey (KeyCode.LeftArrow)) {			//if A or Left arrow is pressed->move left in the world
+			transform.Translate(Vector3.right * -4f * Time.deltaTime, Space.World);
 		}
 		
-		if (Input.GetKey (KeyCode.W) | Input.GetKey (KeyCode.UpArrow)) {//if W or Up arrow is pressed->go up
-			transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.W) | Input.GetKey (KeyCode.UpArrow)) {			//if W or Up arrow is pressed->move up in the world
+			transform.Translate(Vector3.up * 4f * Time.deltaTime, Space.World);
 		}
 		
-		if (Input.GetKey (KeyCode.S) | Input.GetKey (KeyCode.DownArrow) ) {//if S or Down arrow is pressed->go down
-			transform.Translate(Vector3.forward * -3f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S) | Input.GetKey (KeyCode.DownArrow) ) {			//if S or Down arrow is pressed->move down in the world
+			transform.Translate(Vector3.down * 4f * Time.deltaTime, Space.World);
 		}
 }
 
+
 	void RotatePlayer(){
-		Vector3 mousePos = Input.mousePosition;
-		mousePos = cam.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, cam.transform.position.y - transform.position.y));
-		PlayerRotation = Quaternion.LookRotation (mousePos - new Vector3 (transform.position.x, 0, transform.position.z));
+		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;	//Difference of mousepos and playerpos
+		difference.Normalize();																			//makes the sum of Vector3 = 1
 
-
-		//rotation
-		transform.eulerAngles = Vector3.up * 
-			Mathf.MoveTowardsAngle (transform.eulerAngles.y, PlayerRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
-
-
+		float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;						//Calculate the angle to rotate around
+		transform.rotation = Quaternion.Euler(0, 0, rotation - 90);										//Rotates the player
 	}
 }
