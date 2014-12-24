41eff2e9b12f53b764deb066c227bbc23cbe2998
Bart de Haan <Bart.deHaan@hotmail.com>
2014-12-24 19:08:28 +0100

    Menu and texture update
    - Added a game menu
    - Added a gun texture
    - Cleaned the asset folder

---
+++ source/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ChangeSceneTo(string SceneName) {
+		Application.LoadLevel(SceneName);
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+}
